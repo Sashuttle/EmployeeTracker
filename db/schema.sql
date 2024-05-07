0e19e7a13538303098e868e95ae4c641ce6ec6ed
Samantha Shuttleworth <sammi25@Samanthas-Air.attlocal.net>
2024-05-06 21:47:09 -0500

    Added sql code and javascript

--- db/schema.sql
+++ db/schema.sql
@@ -7,15 +7,32 @@ USE business_db;
 
 --Creating Department table
 CREATE TABLE department (
-
+    id SERIAL PRIMARY KEY,
+    department_name VARCHAR(20) NOT NULL
 );
 
 --Creating Role table
 CREATE TABLE role (
-
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(35) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    SET NULL ON DELETE,
+    INDEX (department_id)
 );
 
 --Creating Employee table
 CREATE TABLE employee (
-
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR (35) NOT NULL,
+    last_name VARCHAR (35) NOT NULL,
+    job_title VARCHAR (35) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON UPDATE CASCADE,
+    INDEX (role_id)
 );
--- index.js
+++ index.js
@@ -4,3 +4,52 @@ const mysql = require('mysql');
 const cTable = require ('console.table');
 
 //
+const business_db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'source',
+        password: '',
+        database: 'business_db'
+    },
+)
+
+// View Menu Options
+const options = [
+    {
+        type: 'list',
+        message: 'Please select an option from the menu.',
+        choices: ["View Departments", "View Roles", "View Employees", "Add a Department", "Add a Role", "Add an Employee", "Update Employee Job Title"],
+        name:'optionMenu',
+    },
+]
+
+//Department Question
+const departments = [
+    {
+        type: 'input',
+        message: 'What is the name of the new department?',
+        name: 'newDepartment',
+    },
+]
+
+//Role question
+const role = [
+    {
+        type: 'input',
+        message: 'What title name goes to this new role?',
+        name: 'roleTitle'
+    },
+
+    {
+        type: 'input',
+        message: 'What salary goes with this role?',
+        name: 'roleSalary'
+    },
+
+    {
+        type: 'list',
+        message: 'What department ID goes to the new role?',
+        choices: ['01', '02', '03', '04', '05', '06'],
+        name: 'departmentId',
+    },
+]
